158f04dcb77e8300434adefac272d1943dfc98e8
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-26 17:41:26 +0800

    Add points and card view

--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,54 @@
 import React from 'react';
-import { TouchableOpacity, Image, Button, View, Text } from 'react-native';
-import { Appbar } from 'react-native-paper';
-import { StyleSheet, Platform } from 'react-native';
-
+import { TouchableOpacity, Image, Button, View, Text, StyleSheet } from 'react-native';
+import { Appbar, Card, Paragraph } from 'react-native-paper';
+import IKEA from '../assets/ikea.jpg'
 export default function Home() {
   return (
-    //   <View>
-    //       <Text>Home</Text>
-    //   </View>
-    <Appbar.Header style = {styles.appBarCss}>
-     <Appbar.Content title="Home" />
-      {/* <Appbar.Action icon="magnify" onPress={() => {}} /> */}
-    </Appbar.Header>
+    <>
+      <Appbar.Header style={styles.appBarCss}>
+      <Appbar.Content title="Home" />
+        {/* <Appbar.Action icon="magnify" onPress={() => {}} /> */}
+      </Appbar.Header>
+      <Card>
+        <Card.Title title="Your Points" />
+        <Card.Content style={styles.points}>
+          150
+        </Card.Content>
+      </Card>
+      <Card>
+        <Card.Title title="Quick Discounts Nearby" />
+        <Card style={styles.storeContainer}>
+          <Card.Cover 
+            source={IKEA}
+            resizeMode="cover"
+            style={styles.storeCover} />
+          <Card.Content>
+            <Paragraph style={styles.storeContent}>IKEA</Paragraph>
+          </Card.Content>
+        </Card>
+      </Card>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-    appBarCss: {
-        backgroundColor: 'blue'
-    }
+  appBarCss: {
+    backgroundColor: 'blue'
+  },
+  points: {
+    fontSize: 30,
+    fontWeight: 600,
+    fontFamily: 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  storeContainer: {
+    border: '1px black solid',
+    minHeight: 150,
+    width: 178,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  storeContent: {
+    textAlign: 'center',
+  },
 });
