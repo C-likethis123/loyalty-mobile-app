01f8a3f7801cc97b1cabbc9d542c8f33bf35b2c0
yapdianhao <dianhao.yap@2359media.com>
2020-12-26 16:38:12 +0800

    add about page stuffs

--- screens/About.js
+++ screens/About.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { StyleSheet, Platform } from 'react-native';
 
 export default function About() {
-  return <Text>About</Text>
+  return (
+    <Appbar.Header style = {styles.appBarCss}>
+     <Appbar.Content title="About" />
+    </Appbar.Header>
+  )
 }
+
+const styles = StyleSheet.create({
+    appBarCss: {
+        backgroundColor: 'blue'
+    }
+});
--- screens/Home.js
+++ screens/Home.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { TouchableOpacity, Image, Button, View, Text } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import { StyleSheet, Platform } from 'react-native';
-import App from '../App';
 
 export default function Home() {
   return (
     //   <View>
     //       <Text>Home</Text>
     //   </View>
-    <Appbar.Header style = {styles.item}>
+    <Appbar.Header style = {styles.appBarCss}>
      <Appbar.Content title="Home" />
       {/* <Appbar.Action icon="magnify" onPress={() => {}} /> */}
-  </Appbar.Header>
+    </Appbar.Header>
   );
 }
 
