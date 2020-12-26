02f92cc5e4f8f2738ce379d185ab0a82016294be
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-26 11:03:58 +0800

    Add navigation to app

---
+++ screens/Memberships.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Memberships() {
+  return <Text>Memberships</Text>
+}
---
+++ screens/index.js
@@ -0,0 +1,7 @@
+import About from './About'
+import Home from './Home'
+import Login from './Login'
+import Memberships from './Memberships'
+import PointsTransfer from './PointsTransfer'
+
+export { About, Home, Login, Memberships, PointsTransfer }
